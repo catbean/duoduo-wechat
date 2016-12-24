599687a109d223441c3977977ce0350ff9d9af38
jiajie0531 <jiajie0531@gmail.com>
2016-12-24 15:51:56 +0800

    Initial commit

---
+++ src/test/java/org/jay/DuoduoWechatApplicationTests.java
@@ -0,0 +1,16 @@
+package org.jay;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class DuoduoWechatApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
