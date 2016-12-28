82fdf08295f1c26052b6f270469550d4db23ef30
Jay.Jia <jay.jia@nakedhub.cn>
2016-12-28 16:20:30 +0800

    fix the bug add the service of menu

--- src/main/java/org/jay/service/impl/MenuServiceImpl.java
+++ src/main/java/org/jay/service/impl/MenuServiceImpl.java
@@ -7,7 +7,9 @@ import org.jay.service.MenuService;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 
+@Service
 public class MenuServiceImpl implements MenuService {
 
     private final Logger logger = LoggerFactory.getLogger(this.getClass());
